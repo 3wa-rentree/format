e2cbde045e9fd82b9f839f3c225a5187a2a5e75b
3wa <charlotte.varlet@3wa.io>
2024-09-18 17:49:59 +0200

    script

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 function mysteryFunction1(message, cle) {
-  let resultat = "";for (let i = 0; i < message.length; i++) {resultat += String.fromCharCode(message.charCodeAt(i) ^ cle);}return resultat;}function mysteryFunction2(messageChiffre, cle) {let resultat = "";for (let i = 0; i < messageChiffre.length; i++) {resultat += String.fromCharCode(messageChiffre.charCodeAt(i) ^ cle);}return resultat;}document.getElementById("line").addEventListener("input", (e) => {if (mysteryFunction1(e.target.value.toString(), 15) == "=") {document.getElementById("message").textContent = mysteryFunction2("I`%qwjlvl`h`%fmlccw`%`vq%flkt)%c`ilflqdqljkv",5);document.querySelector("#repo").innerText = mysteryFunction2("lpptw>++cmplqf*gki+7se)vajpvaa+gkjwkha",4);}});
+  let resultat = "";for (let i = 0; i < message.length; i++) {
+      resultat += String.fromCharCode(message.charCodeAt(i) ^ cle);}return resultat;}
+function mysteryFunction2(messageChiffre, cle) {
+    let resultat = "";for (let i = 0; i < messageChiffre.length; i++) {resultat += String.fromCharCode(messageChiffre.charCodeAt(i) ^ cle);}return resultat;}
+document.getElementById("line").addEventListener("input", (e) => {
+    if (mysteryFunction1(e.target.value.toString(), 15) == "=") {
+        document.getElementById("message").textContent = mysteryFunction2("I`%qwjlvl`h`%fmlccw`%`vq%flkt)%c`ilflqdqljkv",5);document.querySelector("#repo").innerText = mysteryFunction2("lpptw>++cmplqf*gki+7se)vajpvaa+gkjwkha",4);}});
