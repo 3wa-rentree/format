81a46fd15ece403b4a0fab326955f0ad3996f67e
3wa <charlotte.varlet@3wa.io>
2024-09-18 17:08:37 +0200

    script

--- number.js
+++
@@ -1,8 +0,0 @@
-// Ex
-function cryptage(message, cle) {
-  let resultat = "";
-  for (let i = 0; i < message.length; i++) {
-    resultat += String.fromCharCode(message.charCodeAt(i) ^ cle);
-  }
-  return resultat;
-}
--- script.js
+++ script.js
@@ -1,26 +1,2 @@
-function cryptage(message, cle) {
-  let resultat = "";
-  for (let i = 0; i < message.length; i++) {
-    resultat += String.fromCharCode(message.charCodeAt(i) ^ cle);
-  }
-  return resultat;
-}
-
-function decryptage(messageChiffre, cle) {
-  let resultat = "";
-  for (let i = 0; i < messageChiffre.length; i++) {
-    resultat += String.fromCharCode(messageChiffre.charCodeAt(i) ^ cle);
-  }
-  return resultat;
-}
-
-document.getElementById("line").addEventListener("input", (e) => {
-  //   TODO : adapter à la ligne, ici, 2
-  if (cryptage(e.target.value.toString(), 15) == "=") {
-    document.getElementById("message").textContent = decryptage(
-      "I`%qwjlvl`h`%fmlccw`%`vq%flkt)%c`ilflqdqljkv",
-      5
-    );
-    document.querySelector("a").href = "https://www.google.com/";
-  }
-});
+function mysteryFunction1(message, cle) {
+  let resultat = "";for (let i = 0; i < message.length; i++) {resultat += String.fromCharCode(message.charCodeAt(i) ^ cle);}return resultat;}function mysteryFunction2(messageChiffre, cle) {let resultat = "";for (let i = 0; i < messageChiffre.length; i++) {resultat += String.fromCharCode(messageChiffre.charCodeAt(i) ^ cle);}return resultat;}document.getElementById("line").addEventListener("input", (e) => {if (mysteryFunction1(e.target.value.toString(), 15) == "=") {document.getElementById("message").textContent = mysteryFunction2("I`%qwjlvl`h`%fmlccw`%`vq%flkt)%c`ilflqdqljkv",5);document.querySelector("#repo").innerText = mysteryFunction2("lpptw>++cmplqf*gki+7se)vajpvaa+gkjwkha",4);}});
